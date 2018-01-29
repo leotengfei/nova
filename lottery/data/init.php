ca7e373dcc0dedec68fb82a2bcdc60853f7c384e
Leo <582970957@qq.com>
2018-01-29 15:07:08 +0800

    Delete init.php

--- lottery/data/init.php
+++
@@ -1,6 +0,0 @@
-<?php
-$username = "dongfei";
-$password = "dongfei816";
-$conn=mysqli_connect('rm-2ze9p3u65h62267nmo.mysql.rds.aliyuncs.com',$username,$password,'wxxk2016',3306);
-mysqli_query($conn,"SET NAMES UTF8");
-?>
