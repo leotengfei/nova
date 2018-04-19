5755695ef5fcd76d3be9e2189220a96363fadb15
Leo <582970957@qq.com>
2018-04-19 14:22:51 +0800

    修改版本，改为卡片式布局

--- video/js/everyday.js
+++ video/js/everyday.js
@@ -2,6 +2,15 @@
 var delDate=function(old_date){
     return old_date.split('/')[0]+'-'+old_date.split('/')[1]+'-'+old_date.split('/')[2];
 }
+function rn(max,min){
+    return Math.floor(Math.random()*(max-min+1)+min);
+}
+function randomColor(){
+    var r=rn(200,100);
+    var g=rn(200,100);
+    var b=rn(200,100);
+    return "rgb("+r+','+g+','+b+")";//所有方法的拼接都可以用ES6新特性`其他字符串{$变量名}`替换
+}
 $.ajax({
     type:'GET',
     data:{
@@ -13,18 +22,20 @@ $.ajax({
         var html='';
         for(var i=0;i<data.length;i++){
             html+=`
-            <li class="media">
+            <div class="col-xs-6 col-md-3">
+                <div style="background-color:${randomColor()};">
                     <a href="${data[i].cid}/${data[i].episode}">
-                        <div class="media-left">
-                            <img class="media-object" src="img/${data[i].imgUrl}" alt="...">
+                        <div class="card_left">
+                            <img src="img/${data[i].imgUrl}">
                         </div>
-                        <div class="media-body">
-                            <h4 class="media-heading">${data[i].cname}</h4>
+                        <div class="card_right">
+                            <h4>${data[i].cname}</h4>
+                            <p>${delDate(data[i].c_time)}</p>
                             <h5>${data[i].description}</h5>
-                            <div>${delDate(data[i].c_time)}</div>
                         </div>
                     </a>
-                </li>
+                </div>
+            </div>
             `;
         }
         $('#evd_list').html(html);
@@ -65,7 +76,7 @@ var getMore=function(){
 getMore();
 
 // evd_list点击事件，跳转到play.html页面
-$('#evd_list').on('click','li>a',function(e){
+$('#evd_list').on('click','div>a',function(e){
     e.preventDefault();
     //console.log($(this).attr('href'));
     var str=$(this).attr('href');
