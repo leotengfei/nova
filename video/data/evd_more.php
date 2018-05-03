15aedc85d28d9f2fd4be1f1824d80f58ef1cf410
Leo <582970957@qq.com>
2018-05-03 14:48:55 +0800

    修改日期查询不正确bug

--- video/data/evd_more.php
+++ video/data/evd_more.php
@@ -5,7 +5,7 @@ require('init.php');
 @$pageNum=$_REQUEST['pageNum'] or die('{"code":-2,"msg":"pageNum是必须的"}');
 $today=date('Y/n/j',time());
 $page=($pageNum-1)*8;
-$sql_getList="SELECT DISTINCT c_time FROM class_detail WHERE c_time<'$today' AND c_time<>'' ORDER BY c_time DESC LIMIT $page,8";
+$sql_getList="SELECT DISTINCT c_time FROM class_detail WHERE unix_timestamp(c_time)<unix_timestamp('$today') AND c_time<>'' ORDER BY unix_timestamp(c_time) DESC LIMIT $page,8";
 $result=mysqli_query($conn,$sql_getList);
 $posts = array();
 while($row = mysqli_fetch_assoc($result)) {
