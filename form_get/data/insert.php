b1d170b3065e6ce400ae542cda88e49cd48e210f
Leo <582970957@qq.com>
2018-06-26 10:18:28 +0800

    修改预约板块，增加短信验证

--- form_get/data/insert.php
+++ form_get/data/insert.php
@@ -2,10 +2,9 @@
 	header("content-type:application/json;charset=utf-8");
 	require('init.php');
 	@$uname=$_REQUEST['uname'] or die('{"code":-2,"msg":"uname是必须的"}');
-	@$utel=$_REQUEST['utel'] or die('{"code":-3,"msg":"utel是必须的"}');
-	@$loca=$_REQUEST['loca'] or die('{"code":-4,"msg":loca是必须的}');
-	@$sub=$_REQUEST['sub'] or die('{"code":-5,"msg":sub是必须的}');
-	$sql="INSERT INTO form_get VALUES(NULL,'$uname','$utel','$loca','$sub')";
+	@$mycode=$_REQUEST['mycode'] or die('{"code":-3,"msg":mycode是必须的}');
+	@$utel=$_REQUEST['utel'] or die('{"code":-4,"msg":"utel是必须的"}');
+	$sql="INSERT INTO form_get VALUES(NULL,'$mycode','$uname','$utel')";
 	$result=mysqli_query($conn,$sql);
 	if($result===true){
 	  echo '{"code":1,"msg":"预约成功"}';
--- form_get/js/index.js
+++ form_get/js/index.js
@@ -1,75 +1,166 @@
 /**
  * Created by Administrator on 2017/10/24 0024.
  */
-
- //flat-ui 初始化checkbox,radio
-$(':checkbox').radiocheck();
-$(':radio').radiocheck();
+// 更换图片方法
+function changing() {
+    document.getElementById('checkpic').src = "img/checkcode.php?" + Math.random();
+}
 
 //初始化姓名，电话判断布尔值
-var vali_uname=false;
-var vali_utel=false;
+var vali_uname = false;
+var vali_utel = false;
+var regname = /^[\u3e00-\u9fa5]{2,5}$/i;
+var regtel = /^[1][3,4,5,7,8,9][0-9]{9}$/i;
+
+var isUtelRepeat = true; //电话号码重复，默认为true
+$('#utel').blur(function () {
+    var utel = $('#utel').val();
+    if (regtel.test(utel)) {
+        // 电话号码格式正确
+        $.ajax({
+            type: 'POST',
+            url: 'data/isphonerepeat.php',
+            data: {
+                utel: utel
+            },
+            success: function (data) {
+                if (data.code == 1) {
+                    // 电话号码可用
+                    isUtelRepeat = false; //改变电话号码为不重复
+                } else {
+                    alert('该电话号码已经预约过，请检查电话号码或更换电话')
+                }
+            }
+        })
+    } else {
+        alert('请检查电话号码格式！')
+    }
+})
+// 获取短信验证码
+$('#sendcode').click(function () {
+    //获取各个input的值
+    var uname = $('#uname').val();
+    var mycode = $('#mycode').val();
+    var utel = $('#utel').val();
+    var checkcode = $('#checkcode').val();
+    var msgcode = $('#msgcode').val();
+    // 判断电话号码是否正确并且不重复
+    if (regtel.test(utel) && !isUtelRepeat) {
+        // 判断图片验证码是否正确
+        var checkcode = $('#checkcode').val().toLowerCase().replace(/\s+/g, ""); //不区分大小写并去除空格
+        $.ajax({
+            type: "GET",
+            url: "data/img_code.php", //验证图片验证码
+            data: {
+                checkcode: checkcode
+            },
+            success: function (data) {
+                if (data.code == "1") {
+                    // 图片验证码正确
+                    $('#sendcode').addClass('disabled');
+                    $('#sendcode').prop('disabled', true);
+                    $.ajax({
+                        type: "GET",
+                        url: "data/api_demo/SmsDemo.php",
+                        data: {
+                            utel: utel
+                        },
+                        success: function (data) {
+                            //console.log(data);
+                            //alert(data);
+                            var second = 60;
+                            var timmer = setInterval(function () {
+                                $('#sendcode').html(second + "秒！");
+                                second--;
+                                //console.log(second);
+                                if (second < 1) {
+                                    clearInterval(timmer);
+                                    changing();
+                                    $('#checkcode').val();
+                                    $('#sendcode').removeClass('disabled');
+                                    $('#sendcode').prop('disabled', false);
+                                    $('#sendcode').html("获取验证码");
+                                }
+                            }, 1000);
+                        },
+                        error: function (data) {
+                            alert("请刷新页面重试！");
+                        }
+                    })
+
+                } else {
+                    alert("图片验证码错误！");
+                }
+            }
+        })
+    } else {
+        // 提示电话号码错误
+        alert('电话号码已经使用过或错误')
+    }
+
+})
 
 $('#btn-apoint').click(function () {
-   //console.log(1);
+    //console.log(1);
     //获取各个input的值
-    var uname=$('#uname').val();
-    var utel=$('#utel').val();
-    var loca=$("input[name='loca']:checked").val();
-    var sub =[];//定义一个数组  
-      $('input[name="subv"]:checked').each(function(){//遍历每一个名字为subv的复选框，其中选中的执行函数  
-        sub.push($(this).val());//将选中的值添加到数组sub中  
-      });
+    var uname = $('#uname').val();
+    var mycode = $('#mycode').val();
+    var utel = $('#utel').val();
+    var checkcode = $('#checkcode').val();
+    var msgcode = $('#msgcode').val();
 
-    console.log(uname,utel);
-    console.log(loca,sub);
+
     //定义正则
-    var regname=/^[\u3e00-\u9fa5]{2,5}$/ig;
-    vali_uname=regname.test(uname);
-    var regtel=/^[1][3,4,5,7,8,9][0-9]{9}$/i;
-    vali_utel=regtel.test(utel);
-    console.log(vali_uname,vali_utel);
-    if(!vali_uname||!vali_utel){
+    vali_uname = regname.test(uname);
+    vali_utel = regtel.test(utel);
+    console.log(vali_uname, vali_utel);
+    if (!vali_uname || !vali_utel) {
         $('#msg').html('请检查姓名或者电话格式！');
         $('#myModal').modal('show');
         return;
     }
-    if(sub===null){
-        $('#msg').html('请至少选择一门考试科目！');
+    if (!mycode) {
+        $('#msg').html('请填写预约号');
         $('#myModal').modal('show');
         return;
     }
-    var my_btn=$(this);
-    my_btn.prop("disabled",true);
-    if(vali_uname&&vali_utel&&loca&&sub){
-        var arr_sub=sub.join(",");
-        console.log(arr_sub);
-        $.ajax({
-            type:'POST',
-            data:{uname:uname,utel:utel,loca:loca,sub:arr_sub},
-            url:'data/insert.php',
-            success: function (data) {
-                console.log(data.msg);
-                var html=`
-                <h4 style="color:#000;" class="text-center">预约成功，请截屏保存测试时间，按时参加！</h4>
-                <ul class="list-unstyled" style="color:#000;" >
-                    <li>北大街：2月28日 下午14：00</li>
-                    <li>长&nbsp;安：2月28日 下午14：00</li>
-                    <li>渭&nbsp;南：3月4日 下午14：00</li>
-                    <li>宝&nbsp;鸡：3月10日 下午19：00</li>
-                    <li>咸&nbsp;阳：2月28日 下午14：00</li>
-                </ul>
-                `;
-                $('#msg').parent().html(html);
-                $('#myModal').modal('show');
+    // 检查短信验证码
+    $.ajax({
+        type: "POST",
+        url: "data/text_code.php",
+        data: {
+            textcode: msgcode
+        },
+        success: function (data) {
+            //console.log(data.msg);
+            if (data.code == '1') { //验证码正确
+                $.ajax({
+                    type: 'POST',
+                    data: {
+                        uname: uname,
+                        mycode: mycode,
+                        utel: utel
+                    },
+                    url: 'data/insert.php',
+                    success: function (data) {
+                        console.log(data.msg);
+                        var html = `
+                            <h4 style="color:#000;" class="text-center">预约成功！</h4>
+                            `;
+                        $('#msg').parent().html(html);
+                        $('#myModal').modal('show');
 
-                $('#uname').val('');
-                $('#utel').val('');
+                        $('#uname').val('');
+                        $('#mycode').val('');
+                        $('#utel').val('');
+                        $('#checkcode').val('');
+                        $('#msgcode').val('');
 
-            },
-            complete: function(){
-                my_btn.prop("disabled",false);
+                    }
+                })
+            } else {
+                alert('请检查短信验证码！');
             }
-        })
-    }
+        }
+    })
 });
