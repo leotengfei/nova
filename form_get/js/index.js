b74efe9af8287b7660e956a5973138333350d823
Leo <582970957@qq.com>
2018-06-27 16:08:46 +0800

    增加高考页面

--- form_get/js/index.js
+++ form_get/js/index.js
@@ -1,10 +1,6 @@
 /**
  * Created by Administrator on 2017/10/24 0024.
  */
-// 更换图片方法
-function changing() {
-    document.getElementById('checkpic').src = "img/checkcode.php?" + Math.random();
-}
 
 //初始化姓名，电话判断布尔值
 var vali_uname = false;
@@ -12,8 +8,9 @@ var vali_utel = false;
 var regname = /^[\u3e00-\u9fa5]{2,5}$/i;
 var regtel = /^[1][3,4,5,7,8,9][0-9]{9}$/i;
 
-var isUtelRepeat = true; //电话号码重复，默认为true
-$('#utel').blur(function () {
+// 获取短信验证码
+$('#sendcode').click(function () {
+    //获取各个input的值
     var utel = $('#utel').val();
     if (regtel.test(utel)) {
         // 电话号码格式正确
@@ -26,37 +23,6 @@ $('#utel').blur(function () {
             success: function (data) {
                 if (data.code == 1) {
                     // 电话号码可用
-                    isUtelRepeat = false; //改变电话号码为不重复
-                } else {
-                    alert('该电话号码已经预约过，请检查电话号码或更换电话')
-                }
-            }
-        })
-    } else {
-        alert('请检查电话号码格式！')
-    }
-})
-// 获取短信验证码
-$('#sendcode').click(function () {
-    //获取各个input的值
-    var uname = $('#uname').val();
-    var mycode = $('#mycode').val();
-    var utel = $('#utel').val();
-    var checkcode = $('#checkcode').val();
-    var msgcode = $('#msgcode').val();
-    // 判断电话号码是否正确并且不重复
-    if (regtel.test(utel) && !isUtelRepeat) {
-        // 判断图片验证码是否正确
-        var checkcode = $('#checkcode').val().toLowerCase().replace(/\s+/g, ""); //不区分大小写并去除空格
-        $.ajax({
-            type: "GET",
-            url: "data/img_code.php", //验证图片验证码
-            data: {
-                checkcode: checkcode
-            },
-            success: function (data) {
-                if (data.code == "1") {
-                    // 图片验证码正确
                     $('#sendcode').addClass('disabled');
                     $('#sendcode').prop('disabled', true);
                     $.ajax({
@@ -75,8 +41,6 @@ $('#sendcode').click(function () {
                                 //console.log(second);
                                 if (second < 1) {
                                     clearInterval(timmer);
-                                    changing();
-                                    $('#checkcode').val();
                                     $('#sendcode').removeClass('disabled');
                                     $('#sendcode').prop('disabled', false);
                                     $('#sendcode').html("获取验证码");
@@ -87,15 +51,13 @@ $('#sendcode').click(function () {
                             alert("请刷新页面重试！");
                         }
                     })
-
                 } else {
-                    alert("图片验证码错误！");
+                    alert('该电话号码已经预约过，请检查电话号码或更换电话')
                 }
             }
         })
     } else {
-        // 提示电话号码错误
-        alert('电话号码已经使用过或错误')
+        alert('请检查电话号码格式！')
     }
 
 })
@@ -104,9 +66,8 @@ $('#btn-apoint').click(function () {
     //console.log(1);
     //获取各个input的值
     var uname = $('#uname').val();
-    var mycode = $('#mycode').val();
     var utel = $('#utel').val();
-    var checkcode = $('#checkcode').val();
+    var mycode = $('#mycode').val();
     var msgcode = $('#msgcode').val();
 
 
