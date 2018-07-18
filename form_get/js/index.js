3f7cf7569be7a4b35ef4b6a31426485cc7221d8a
Leo <582970957@qq.com>
2018-07-18 11:57:57 +0800

    增加上传文件功能

--- form_get/js/index.js
+++ form_get/js/index.js
@@ -7,69 +7,159 @@ var vali_uname = false;
 var vali_utel = false;
 var regname = /^[\u3e00-\u9fa5]{2,5}$/i;
 var regtel = /^[1][3,4,5,7,8,9][0-9]{9}$/i;
+var arr=[];
+var fileSelectNum = 0;
+
+
+$('#file-fr').fileinput({
+    theme: 'fa',
+    language: 'zh',
+    showCaption: false,
+    showRemove: false,
+    uploadUrl: 'https://mokey.club/adminUploads/upload',
+    maxFilesNum: 10,
+    dropZoneEnabled: false,//是否显示拖拽区域
+    browseClass: "btn btn-primary btn-block",
+    uploadClass:"btn btn-warning btn-block",
+    allowedFileExtensions: ['bmp','jpeg','jpg', 'png', 'gif','tiff','webp','svg']
+});
+$("#file-fr").on("filebatchselected", function(event, files) {
+    fileSelectNum = files.length
+    console.log(fileSelectNum)
+});
+$("#file-fr").on("fileuploaded", function (event, data, previewId, index) {
+    //console.log(data.response)
+    arr=arr.concat(data.response)
+    console.log(arr)
+});
 
 // 获取短信验证码
-$('#sendcode').click(function () {
-    //获取各个input的值
-    var utel = $('#utel').val();
-    if (regtel.test(utel)) {
-        // 电话号码格式正确
-        $.ajax({
-            type: 'POST',
-            url: 'data/isphonerepeat.php',
-            data: {
-                utel: utel
-            },
-            success: function (data) {
-                if (data.code == 1) {
-                    // 电话号码可用
-                    $('#sendcode').addClass('disabled');
-                    $('#sendcode').prop('disabled', true);
-                    $.ajax({
-                        type: "GET",
-                        url: "data/api_demo/SmsDemo.php",
-                        data: {
-                            utel: utel
-                        },
-                        success: function (data) {
-                            //console.log(data);
-                            //alert(data);
-                            var second = 60;
-                            var timmer = setInterval(function () {
-                                $('#sendcode').html(second + "秒！");
-                                second--;
-                                //console.log(second);
-                                if (second < 1) {
-                                    clearInterval(timmer);
-                                    $('#sendcode').removeClass('disabled');
-                                    $('#sendcode').prop('disabled', false);
-                                    $('#sendcode').html("获取验证码");
-                                }
-                            }, 1000);
-                        },
-                        error: function (data) {
-                            alert("请刷新页面重试！");
-                        }
-                    })
-                } else {
-                    alert('该电话号码已经预约过，请检查电话号码或更换电话')
-                }
-            }
-        })
-    } else {
-        alert('请检查电话号码格式！')
-    }
+// $('#sendcode').click(function () {
+//     //获取各个input的值
+//     var utel = $('#utel').val();
+//     if (regtel.test(utel)) {
+//         // 电话号码格式正确
+//         $.ajax({
+//             type: 'POST',
+//             url: 'data/isphonerepeat.php',
+//             data: {
+//                 utel: utel
+//             },
+//             success: function (data) {
+//                 if (data.code == 1) {
+//                     // 电话号码可用
+//                     $('#sendcode').addClass('disabled');
+//                     $('#sendcode').prop('disabled', true);
+//                     $.ajax({
+//                         type: "GET",
+//                         url: "data/api_demo/SmsDemo.php",
+//                         data: {
+//                             utel: utel
+//                         },
+//                         success: function (data) {
+//                             //console.log(data);
+//                             //alert(data);
+//                             var second = 60;
+//                             var timmer = setInterval(function () {
+//                                 $('#sendcode').html(second + "秒！");
+//                                 second--;
+//                                 //console.log(second);
+//                                 if (second < 1) {
+//                                     clearInterval(timmer);
+//                                     $('#sendcode').removeClass('disabled');
+//                                     $('#sendcode').prop('disabled', false);
+//                                     $('#sendcode').html("获取验证码");
+//                                 }
+//                             }, 1000);
+//                         },
+//                         error: function (data) {
+//                             alert("请刷新页面重试！");
+//                         }
+//                     })
+//                 } else {
+//                     alert('该电话号码已经预约过，请检查电话号码或更换电话')
+//                 }
+//             }
+//         })
+//     } else {
+//         alert('请检查电话号码格式！')
+//     }
 
-})
+// })
 
+// $('#btn-apoint').click(function () {
+//     //console.log(1);
+//     //获取各个input的值
+//     var uname = $('#uname').val();
+//     var utel = $('#utel').val();
+//     var mycode = $('#mycode').val();
+//     var msgcode = $('#msgcode').val();
+
+
+//     //定义正则
+//     vali_uname = regname.test(uname);
+//     vali_utel = regtel.test(utel);
+//     console.log(vali_uname, vali_utel);
+//     if (!vali_uname || !vali_utel) {
+//         $('#msg').html('请检查姓名或者电话格式！');
+//         $('#myModal').modal('show');
+//         return;
+//     }
+//     if (!mycode) {
+//         $('#msg').html('请填写预约号');
+//         $('#myModal').modal('show');
+//         return;
+//     }
+//     // 检查短信验证码
+//     $.ajax({
+//         type: "POST",
+//         url: "data/text_code.php",
+//         data: {
+//             textcode: msgcode
+//         },
+//         success: function (data) {
+//             //console.log(data.msg);
+//             if (data.code == '1') { //验证码正确
+//                 $.ajax({
+//                     type: 'POST',
+//                     data: {
+//                         uname: uname,
+//                         mycode: mycode,
+//                         utel: utel
+//                     },
+//                     url: 'data/insert.php',
+//                     success: function (data) {
+//                         console.log(data.msg);
+//                         var html = `
+//                             <h4 style="color:rgb(8, 156, 94);" class="text-center">预约成功！请截屏保存结果</h4>
+//                             <img src="img/biaoge.png" alt="" class="chart">
+//                             `;
+//                         $('#msg').parent().html(html);
+//                         $('#myModal').modal('show');
+
+//                         $('#uname').val('');
+//                         $('#mycode').val('');
+//                         $('#utel').val('');
+//                         $('#checkcode').val('');
+//                         $('#msgcode').val('');
+
+//                     }
+//                 })
+//             } else {
+//                 alert('请检查短信验证码！');
+//             }
+//         }
+//     })
+// });
 $('#btn-apoint').click(function () {
     //console.log(1);
     //获取各个input的值
     var uname = $('#uname').val();
     var utel = $('#utel').val();
-    var mycode = $('#mycode').val();
-    var msgcode = $('#msgcode').val();
-
+    var loca = $('#loca').val();
+    var imgArr = arr.join(',')
+    console.log(uname,utel,loca)
+    console.log(imgArr)
 
     //定义正则
     vali_uname = regname.test(uname);
@@ -80,49 +170,36 @@ $('#btn-apoint').click(function () {
         $('#myModal').modal('show');
         return;
     }
-    if (!mycode) {
-        $('#msg').html('请填写预约号');
+    if (arr.length===0) {
+        $('#msg').html('请上传自己的笔记图片！');
         $('#myModal').modal('show');
         return;
     }
-    // 检查短信验证码
+
+    // 插入信息
     $.ajax({
-        type: "POST",
-        url: "data/text_code.php",
+        type: 'POST',
         data: {
-            textcode: msgcode
+            uname: uname,
+            utel: utel,
+            loca:loca,
+            img:imgArr
         },
+        url: 'data/insert.php',
         success: function (data) {
-            //console.log(data.msg);
-            if (data.code == '1') { //验证码正确
-                $.ajax({
-                    type: 'POST',
-                    data: {
-                        uname: uname,
-                        mycode: mycode,
-                        utel: utel
-                    },
-                    url: 'data/insert.php',
-                    success: function (data) {
-                        console.log(data.msg);
-                        var html = `
-                            <h4 style="color:rgb(8, 156, 94);" class="text-center">预约成功！请截屏保存结果</h4>
-                            <img src="img/biaoge.png" alt="" class="chart">
-                            `;
-                        $('#msg').parent().html(html);
-                        $('#myModal').modal('show');
+            console.log(data.msg);
+            var html = `
+                <h4 style="color:rgb(8, 156, 94);" class="text-center">报名成功！</h4>
+                <h6 style="text-align:center;color:#333;">具体结果请关注无线星空官网wxxk.org</h6>
+                <h6 style="text-align:center;color:#333;">或官方微信公众号</h6>
+                <p style="text-align:center;"><img src="img/wx.jpg" style="width:100%;"></p>
+                `;
+            $('#msg').parent().html(html);
+            $('#myModal').modal('show');
 
-                        $('#uname').val('');
-                        $('#mycode').val('');
-                        $('#utel').val('');
-                        $('#checkcode').val('');
-                        $('#msgcode').val('');
-
-                    }
-                })
-            } else {
-                alert('请检查短信验证码！');
-            }
+            $('#uname').val('');
+            $('#utel').val('');
+            $('#loca').val('');
         }
     })
 });
